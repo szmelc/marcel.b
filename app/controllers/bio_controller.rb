0e73b3ad4e1cda742b38d3aa1b9c2a3be335d014
Łukasz Szmelc <lukasz.szmelc@netguru.pl>
2019-10-09 22:17:50 +0200

    Add bio page

---
+++ app/controllers/bio_controller.rb
@@ -0,0 +1,3 @@
+class BioController < ApplicationController
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|pl/ do
     root to: 'homepage#index'
+    get '/bio', to: 'bio#index'
     get '/concerts', to: 'concerts#index'
   end
   devise_for :admin_users, ActiveAdmin::Devise.config
