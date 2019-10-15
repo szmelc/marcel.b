75fc344e033dbad5c7d32096da13695850194468
Łukasz Szmelc <lukasz.szmelc@netguru.pl>
2019-10-15 20:42:34 +0200

    Add pages templates

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
     root to: 'homepage#index'
     get '/bio', to: 'bio#index'
     get '/concerts', to: 'concerts#index'
+    get '/contact', to: 'contact#index'
     get '/gallery', to: 'gallery#index'
+    get '/press', to: 'press#index'
     get '/projects', to: 'projects#index'
   end
   devise_for :admin_users, ActiveAdmin::Devise.config
