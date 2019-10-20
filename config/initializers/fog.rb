916c3dc9deb95181b52e92b944c108a70fc983a9
Łukasz Szmelc <lukasz.szmelc@netguru.pl>
2019-10-20 10:58:59 +0200

    Enable S3 photos hosting

---
+++ config/initializers/fog.rb
@@ -0,0 +1,18 @@
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  # if Rails.env.production?
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    :provider => 'AWS',
+    :aws_access_key_id => Rails.application.credentials.aws[:access_key_id],
+    :aws_secret_access_key => Rails.application.credentials.aws[:secret_access_key],
+    :region => 'eu-central-1'
+  }
+  config.fog_directory = Rails.application.credentials.aws[:s3_bucket_name]
+  config.storage = :fog
+  # else
+  #   config.storage = :file
+  #   config.enable_processing = Rails.env.development?
+  # end
+end
