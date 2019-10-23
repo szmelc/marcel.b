4a012389a04f1b8b688c82ca98b868bbaabbb367
Łukasz Szmelc <lukasz.szmelc@netguru.pl>
2019-10-23 15:34:22 +0200

    Show only upcoming concerts in homepage

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -3,5 +3,5 @@ class Concert < ApplicationRecord
   scope :in_future_years, -> { where("date > ?", Date.new(Date.today.year + 1)) }
   scope :in_year, ->(year) { where(date: Date.new(year)..Date.new(year).end_of_year) }
   scope :recent, -> { order('date desc').limit(8) }
-  scope :upcoming, -> { order('date asc').limit(8) }
+  scope :upcoming, -> { where('date > ?', Date.today).order('date asc').limit(8) }
 end
