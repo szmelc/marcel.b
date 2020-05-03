4a27b54d91ff75ad5f0104343fbbd607529caaa7
Lukasz Szmelc <lszmelc@gmail.com>
2020-05-03 23:30:44 +0200

    Fix indendation

--- app/admin/news.rb
+++ app/admin/news.rb
@@ -1,31 +1,31 @@
 ActiveAdmin.register News do
-    permit_params :polish_content, :polish_title, :english_content,
-                  :english_title, :locale
+  permit_params :polish_content, :polish_title, :english_content,
+                :english_title, :locale
 
-    index do
-      selectable_column
-      id_column
-      column :polish_title
-      column :english_title
-      actions
-    end
+  index do
+    selectable_column
+    id_column
+    column :polish_title
+    column :english_title
+    actions
+  end
 
-    show do |project|
-      attributes_table do
-        row :polish_title
-        row :english_title
-        row :polish_content
-        row :english_content
-      end
-    end
-
-    form do |f|
-      f.inputs do
-        f.input :polish_title
-        f.input :english_title
-        f.rich_text_area :english_content, as: :quill_editor
-        f.rich_text_area :polish_content, as: :quill_editor
-      end
-      f.actions
+  show do |project|
+    attributes_table do
+      row :polish_title
+      row :english_title
+      row :polish_content
+      row :english_content
     end
   end
+
+  form do |f|
+    f.inputs do
+      f.input :polish_title
+      f.input :english_title
+      f.rich_text_area :english_content, label: 'Polish Content'
+      f.rich_text_area :polish_content, label: 'English Content'
+    end
+    f.actions
+  end
+end
